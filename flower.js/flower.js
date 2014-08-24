e6b1f863649ffa2ce2a8c9548c204e512924ba09
Anugrah Bagus S <newbieilmu@gmail.com>
2014-08-24 14:27:21 +0700

    Create flower.js

---
+++ flower.js/flower.js
@@ -0,0 +1,50 @@
+// by Jason Rowe - jasonrowe.com @jsonrow
+function Flower(thee, numberOfPetals) {
+    var petals = numberOfPetals;
+
+    if(!petals && petals !== 0){
+        petals = Math.floor(Math.random()* 5) + 5;
+    }
+
+    var originalTotalNumberOfPetals = petals;
+
+    this.totalPetals = function () {
+        return originalTotalNumberOfPetals;
+    }
+
+    this.removePetal = function(){
+        if(petals > 0){
+            petals = petals - 1;
+        }
+        
+        return petals;
+    };
+
+    this.petals = function(){
+        return petals;
+    };
+
+    var love = false;
+
+    this.toggleLove = function(){
+        love = !love;
+        return love;
+    };
+
+    this.isItLove = function(){
+        return love;
+    };
+
+    this.result = function(){
+        var message;
+
+        if(this.toggleLove()){
+            message = thee + " Terima";
+        }
+        else{
+            message = thee + " Tolak";
+        }
+
+        return message;
+    };
+}
